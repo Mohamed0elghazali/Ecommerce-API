8e59bc8bdaaaf7100b334f95bda56079296d596d
melg <melg@melg.com>
2024-01-27 21:01:02 +0200

    Adding Serializers

--- product/models.py
+++ product/models.py
@@ -14,8 +14,12 @@ class Product(models.Model):
     description = models.TextField(max_length=1000, default="", blank=False)
     price = models.DecimalField(max_digits=7, decimal_places=2, default=0)
     brand = models.CharField(max_length=200, default="", blank=False)
-    categroy = models.CharField(max_length=40,  choices=Category.choices)
+    category = models.CharField(max_length=40,  choices=Category.choices)
     ratings = models.DecimalField(max_digits=3, decimal_places=2, default=0)
     stock = models.IntegerField(default=0)
     createAt = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    # return object name in admin panel and object name.
+    def __str__(self):
+        return self.name
--- product/views.py
+++ product/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
 
@@ -11,6 +12,8 @@ from .models import Product
 def get_all_products(request):
     products = Product.objects.all()
     print(products)
+    serializer = ProductSerializer(products, many=True)
+    print(serializer.data)
     return Response(
-        {"products": "Put Products Here",}
+        {"products": serializer.data}
     )
