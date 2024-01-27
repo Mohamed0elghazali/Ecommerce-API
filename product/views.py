5853c3f7775517382ea138171c295c1c8cd3417e
melg <melg@melg.com>
2024-01-27 20:46:03 +0200

    making a get endpoint to return all products

--- product/views.py
+++ product/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Product
+
 # Create your views here.
+
+@api_view(["GET"])
+def get_all_products(request):
+    products = Product.objects.all()
+    print(products)
+    return Response(
+        {"products": "Put Products Here",}
+    )
