d82e938911f0067ca449db46cf57939f0b0358ef
melg <melg@melg.com>
2024-01-29 22:16:08 +0200

    adding pagination

--- product/urls.py
+++ product/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('products/', views.get_all_products, name='products'),
     path('products/<str:pk>/', views.get_product, name='get_by_id'),
     path('products_byfilter/', views.get_by_filter, name='get_by_filter'),
+    path('products_bypage/', views.get_by_page, name='get_by_page'),
 ]
--- product/views.py
+++ product/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from rest_framework.pagination import PageNumberPagination
 from .models import Product
 from .serializers import ProductSerializer
 from .fitlers import ProductFiter
@@ -34,4 +34,19 @@ def get_by_filter(request):
     serializer = ProductSerializer(filterset.qs, many=True)
     return Response(
         {"products": serializer.data}
+    )
+
+@api_view(["GET"])
+def get_by_page(request):
+    filterset = ProductFiter(request.GET, queryset=Product.objects.all())
+    count = filterset.qs.count()
+    
+    resPage = 2
+    paginator = PageNumberPagination()
+    paginator.page_size = resPage
+
+    queryset = paginator.paginate_queryset(filterset.qs, request)
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(
+        {"Product": serializer.data, "Per Page": resPage, "Count": count}
     )
