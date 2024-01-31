a908a959cc4eafe1f319a946aadc38a44ba1f23e
melg <melg@melg.com>
2024-01-31 22:18:05 +0200

    adding post-update-delete endpoints

--- product/urls.py
+++ product/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('products/<str:pk>/', views.get_product, name='get_by_id'),
     path('products_byfilter/', views.get_by_filter, name='get_by_filter'),
     path('products_bypage/', views.get_by_page, name='get_by_page'),
+    path('products/new', views.new_product, name='new_product'),
+    path('products/update/<str:pk>', views.update_product, name='update_product'),
+    path('products/delete/<str:pk>', views.delete_product, name='delete_product'),
 ]
